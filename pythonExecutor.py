e7c2df5f35bc24a825280ba166b6ed3caffdcb6a
saksham.Gupta98 <sakshamgupta026@gmail.com>
2022-05-10 21:05:15 +0530

    test migrations new schema

--- pythonExecutor.py
+++ pythonExecutor.py
@@ -1,3 +1,3 @@
 import subprocess
-result = subprocess.run(["schemachange", "-f", "$GITHUB_WORKSPACE/migrations", "-a", "$SF_ACCOUNT", "-u", "$SF_USERNAME", "-r", "$SF_ROLE", "-w", "$SF_WAREHOUSE", "-d", "$SF_DATABASE", "-c", "$SF_DATABASE.demo.CHANGE_HISTORY", "--create-change-history-table"], stderr=subprocess.PIPE, text=True)
+result = subprocess.run(["schemachange", "-f", "/migrations", "-a", "$SF_ACCOUNT", "-u", "$SF_USERNAME", "-r", "$SF_ROLE", "-w", "$SF_WAREHOUSE", "-d", "$SF_DATABASE", "-c", "$SF_DATABASE.demo.CHANGE_HISTORY", "--create-change-history-table"], stderr=subprocess.PIPE, text=True)
 print(result.stderr)
