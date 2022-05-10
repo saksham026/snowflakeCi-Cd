42a6297b5fc318be3b51b4bc126a22cc3cc5e293
saksham.Gupta98 <sakshamgupta026@gmail.com>
2022-05-10 21:37:03 +0530

    test migrations new schema

--- pythonExecutor.py
+++ pythonExecutor.py
@@ -11,5 +11,12 @@ os.environ["SNOWFLAKE_PASSWORD"] = "bigData123$"
 
 print(sys.argv[1])
 
+n = len(sys.argv)
+
+print(n)
+
+for i in range(1, n):
+    print(sys.argv[i])
+
 result = subprocess.run(["schemachange", "-f", "/home/runner/work/snowflakeCi-Cd/snowflakeCi-Cd/migrations", "-a", "elysiumpartner", "-u", "app", "-r", "SYSADMIN", "-w", "demo_wh", "-d", "tenant3", "-c", "tenant3.demo.CHANGE_HISTORY", "--create-change-history-table"], stderr=subprocess.PIPE, text=True)
 print(result.stderr)
